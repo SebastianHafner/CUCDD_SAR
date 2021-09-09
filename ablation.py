f382e88f24abebefdd3a663578e2235e20061b49
SebastianHafner <hafnersailing@gmail.com>
2021-09-09 19:06:35 +0200

    yes

--- ablation.py
+++ ablation.py
@@ -62,12 +62,44 @@ def ablation1(error_multiplier: int, min_diff_range: tuple, step_size: float, ba
     plt.show()
 
 
-
 def ablation2(min_diff: float, error_multiplier_range: tuple, step_size: float, band: str = 'VV'):
-    pass
+    error_multiplier_start, error_multiplier_end = error_multiplier_range
+    error_multiplier_candidates = np.arange(error_multiplier_start, error_multiplier_end + step_size, step_size)
+    file = config.root_path() / 'ablation' / f'ablation2_{band}_{min_diff:.1f}.json'
+
+    if file.exists():
+        ablation_data = geofiles.load_json(file)
+    else:
+        ablation_data = {
+            'error_multiplier_range': error_multiplier_range,
+            'step_size': step_size,
+            'f1_score': [],
+            'precision': [],
+            'recall': []
+        }
+
+        for error_multiplier_candidate in tqdm(error_multiplier_candidates):
+            sf = cd_models.StepFunctionModel(band, error_multiplier=error_multiplier_candidate, min_diff=min_diff,
+                                             min_segment_length=2)
+            f1, precision, recall = quanitative_evaluation(sf)
+            ablation_data['f1_score'].append(f1)
+            ablation_data['precision'].append(precision)
+            ablation_data['recall'].append(recall)
+        geofiles.write_json(file, ablation_data)
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    ax.plot(error_multiplier_candidates, ablation_data['f1_score'], label='F1 score')
+    ax.plot(error_multiplier_candidates, ablation_data['precision'], label='Precision')
+    ax.plot(error_multiplier_candidates, ablation_data['recall'], label='Recall')
+
+    ax.set_xlim([error_multiplier_start, error_multiplier_end])
+    ax.set_ylim([0, 1])
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
-    ablation1(3, min_diff_range=(0, 10), step_size=1)
+    # ablation1(3, min_diff_range=(0, 10), step_size=1)
+    ablation2(2, error_multiplier_range=(0, 5), step_size=0.5)
 
 
